e8bed2a2e00c7ba3d792bc524bf904e5dfb080d2
JORGE DANIEL JIMENEZ BECERRA <jorgetrabajo02@gmail.com>
2019-04-09 03:04:52 -0500

    Carpeta con toda la pagina Revision dia 04/09/2019

---
+++ Web/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/house')
+def house():
+    return render_template('house.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
